4559cb060ffe951ebd6638f37353ced1a6426a3b
MCastro34 <m.goncalves.castro@gmail.com>
2021-04-19 13:05:29 +0100

    Add earth wall

--- Magic Test/Assets/Scripts/Player/MagicController.cs
+++ Magic Test/Assets/Scripts/Player/MagicController.cs
@@ -4,19 +4,29 @@ public class MagicController : MonoBehaviour
 {
     public CharacterBehaviour Player;
     public ThirdPersonMovement characterMovement;
+
     public GameObject Fireball;
+    public GameObject Earthwall;
+
     public Transform cam;
+    
+    GameObject earthwall;
+    float freezeTimer = 0.5f;
+
     public float fireBallCooldown = 5f;
     public float waterHealCooldown = 10f;
+    public float earthWallCooldown = 5f;
 
     float activeElement = 1f;
     float fbCooldown;
     float whCooldown;
+    float ewCooldown;
 
     void Start()
     {
         fbCooldown = fireBallCooldown;
         whCooldown = waterHealCooldown;
+        ewCooldown = earthWallCooldown;
         characterMovement = GameObject.FindObjectOfType<ThirdPersonMovement>();
     }
 
@@ -46,6 +56,34 @@ public class MagicController : MonoBehaviour
         {
             characterMovement.HandleDash();
         }
-        
+
+        //Raise Wall
+        EarthwallRaise();
+    }
+
+    void EarthwallRaise()
+    {
+        ewCooldown -= Time.deltaTime;
+        if(Input.GetKeyDown(KeyCode.E) && ewCooldown <= 0f)
+        {
+            Vector3 offset = cam.forward * 5;
+            offset.y=0;
+            GetComponent<ThirdPersonMovement>().enabled = false;
+            Earthwall.transform.localScale = new Vector3(1, 0.01f, 1);
+            earthwall = Instantiate(Earthwall, new Vector3(transform.position.x, 0.005f, transform.position.z) + offset, Quaternion.Euler(0f, cam.eulerAngles.y, 0f));
+            freezeTimer = 0.5f;
+            ewCooldown = earthWallCooldown;
+        }
+
+        if(earthwall && earthwall.transform.localScale.y < 1)
+        {
+            earthwall.transform.localScale += new Vector3(0, 0.01f, 0);
+        }
+
+        if(freezeTimer > 0) freezeTimer -= Time.deltaTime;
+        if(!GetComponent<ThirdPersonMovement>().enabled && freezeTimer < 0)
+        {
+            GetComponent<ThirdPersonMovement>().enabled = true;
+        }
     }
 }
