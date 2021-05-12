99790862a3ad61208beaa1b2447adf2ebf17747e
UnlimitedSpeed <joaomonteiromail@gmail.com>
2021-05-12 19:29:13 +0100

    Update scenes

--- Magic Test/Assets/SpellBook.cs
+++ Magic Test/Assets/SpellBook.cs
@@ -11,14 +11,14 @@ public class SpellBook : MonoBehaviour
     void Start()
     {
         selectedMagics = controller.GetMagics();
-        foreach (Magic currentSprite in selectedMagics)
+        /*foreach (Magic currentSprite in selectedMagics)
         {
             GameObject NewObj = new GameObject(); //Create the GameObject
             Image NewImage = NewObj.AddComponent<Image>(); //Add the Image Component script
             NewImage.sprite = currentSprite; //Set the Sprite of the Image Component on the new GameObject
             NewObj.GetComponent<RectTransform>().SetParent(ParentPanel.transform); //Assign the newly created Image GameObject as a Child of the Parent Panel.
             NewObj.SetActive(true); //Activate the GameObject
-        }
+        }*/
     }
 
     void Update()
