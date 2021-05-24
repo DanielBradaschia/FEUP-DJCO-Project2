08ddf4b074ee36f9f4dfffb4c52d20976380d082
UnlimitedSpeed <joaomonteiromail@gmail.com>
2021-05-24 15:51:24 +0100

    Updated Magics and scenery

--- Magic Test/Assets/Magic/Fire/Flamethrower/Flamethrower.cs
+++ Magic Test/Assets/Magic/Fire/Flamethrower/Flamethrower.cs
@@ -3,8 +3,39 @@ using UnityEngine;
 public class Flamethrower : MonoBehaviour
 {
     public float damage = 0.2f;
+    public GameObject flame;
 
-    private void OnParticleCollision(GameObject other)
+    GameObject camera;
+    Vector3 hit;
+    Vector3 direction;
+
+    void Start()
+    {
+        camera = GameObject.Find("Camera");
+    }
+
+    void Update()
+    {
+        Ray rayOrigin = new Ray(camera.transform.position, camera.transform.forward);
+
+        Vector3 aimPoint;
+        RaycastHit hit;
+        if (Physics.Raycast(rayOrigin, out hit))
+        {
+            aimPoint = hit.point;
+            
+        }
+        else
+        {
+            aimPoint = rayOrigin.origin + rayOrigin.direction * 1000f;
+        }
+        
+        direction = aimPoint - transform.position;
+        
+        transform.rotation = Quaternion.LookRotation(direction);
+    }
+
+    void OnParticleCollision(GameObject other)
     {
         if (other.tag == "Enemy")
         {
