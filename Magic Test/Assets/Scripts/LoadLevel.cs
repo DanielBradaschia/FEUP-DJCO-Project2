b2cb8b3e8b58804cc649edbe6b2ab5d29ae21f98
Daniel Gazola Bradaschia <daniel.bradaschia@gmail.com>
2021-06-06 16:27:13 +0100

    Create Portal Script

---
+++ Magic Test/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+
+    public string level;
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            SceneManager.LoadScene(level);
+    }
+}
